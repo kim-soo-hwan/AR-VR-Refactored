86e26ed2cd70927d1380d2c3cc934290f3c14e8b
Soohwan Kim <kimsoohwan@sunmoon.ac.kr>
2022-06-07 21:35:15 +0900

    Fixed some bugs on macOS

--- projects/Pointcloud_Visualization/05_Intel_RealSense_Capture/main.cpp
+++ projects/Pointcloud_Visualization/05_Intel_RealSense_Capture/main.cpp
@@ -1,39 +1,36 @@
 // License: Apache 2.0. See LICENSE file in root directory.
-// Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+// Copyright(c) 2019 Intel Corporation. All Rights Reserved.
 
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
-#include "example.hpp"          // Include short list of convenience functions for rendering
+#include <iostream>             // for cout
 
-// Capture Example demonstrates how to
-// capture depth and color video streams and render them to the screen
+// Hello RealSense example demonstrates the basics of connecting to a RealSense device
+// and taking advantage of depth data
 int main(int argc, char * argv[]) try
 {
-    rs2::log_to_console(RS2_LOG_SEVERITY_ERROR);
-    // Create a simple OpenGL window for rendering:
-    window app(1280, 720, "RealSense Capture Example");
+    // Create a Pipeline - this serves as a top-level API for streaming and processing frames
+    rs2::pipeline p;
 
-    // Declare depth colorizer for pretty visualization of depth data
-    rs2::colorizer color_map;
-    // Declare rates printer for showing streaming rates of the enabled streams.
-    rs2::rates_printer printer;
+    // Configure and start the pipeline
+    p.start();
 
-    // Declare RealSense pipeline, encapsulating the actual device and sensors
-    rs2::pipeline pipe;
-
-    // Start streaming with default recommended configuration
-    // The default video configuration contains Depth and Color streams
-    // If a device is capable to stream IMU data, both Gyro and Accelerometer are enabled by default
-    pipe.start();
-
-    while (app) // Application still alive?
+    while (true)
     {
-        rs2::frameset data = pipe.wait_for_frames().    // Wait for next set of frames from the camera
-                             apply_filter(printer).     // Print each enabled stream frame rate
-                             apply_filter(color_map);   // Find and colorize the depth data
+        // Block program until frames arrive
+        rs2::frameset frames = p.wait_for_frames();
 
-        // The show method, when applied on frameset, break it to frames and upload each frame into a gl textures
-        // Each texture is displayed on different viewport according to it's stream unique id
-        app.show(data);
+        // Try to get a frame of a depth image
+        rs2::depth_frame depth = frames.get_depth_frame();
+
+        // Get the depth frame's dimensions
+        auto width = depth.get_width();
+        auto height = depth.get_height();
+
+        // Query the distance from the camera to the object in the center of the image
+        float dist_to_center = depth.get_distance(width / 2, height / 2);
+
+        // Print the distance
+        std::cout << "The camera is facing an object " << dist_to_center << " meters away \r";
     }
 
     return EXIT_SUCCESS;
